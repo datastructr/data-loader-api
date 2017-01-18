080908c5d83de5c3944c8a7f7ade947243da1e69
Jordan Taylor <jtaylor007.jt@gmail.com>
2017-01-18 15:22:45 -0500

    new PATH changes

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
-PATH=$WORKSPACE/env/bin:/usr/local/bin:$PATH
-if [ ! -d "env" ]; then
-        pyvenv env
-fi
+# Adding virtualenv to PATH
+PATH=/usr/local/bin:$PATH
+cd $WORKSPACE/
+
+pyvenv --no-site-packages env
 source env/bin/activate
+
 pip install -r requirements.txt --download-cache=/tmp/$JOB_NAME
 
 py.test
