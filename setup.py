6f3af92cd747766d04dc365d9be2882812223474
Jordan Taylor <jtaylor007.jt@gmail.com>
2017-01-18 16:36:46 -0500

    more configs

--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@ from setuptools import setup, find_packages
 
 
 setup(
-    packages=find_packages(),
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    packages=find_packages(exclude=('tests',)),
+    setup_requires=[
+        'pytest-runner>=2.0'
+    ],
+    tests_require=[
+        'pytest>=3.0',
+        'pytest-cov>=2.4'
+    ]
 )
